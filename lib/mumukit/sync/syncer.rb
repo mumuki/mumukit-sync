962661aeb601ffa85997a63843e7d590d8836b27
Franco Bulgarelli <franco@mumuki.org>
2018-11-03 15:37:30 -0300

    Fixing choices support

--- lib/mumukit/sync/syncer.rb
+++ lib/mumukit/sync/syncer.rb
@@ -63,12 +63,23 @@ module Mumukit::Sync
 end
 
 module Mumukit::Sync::Inflator
-  class Choice
+  class Exercise
     def inflate!(key, resource_h)
       return unless key.kind == :guide
-      return unless resource_h[:language][:name] == 'text'
-      return unless resource_h[:editor] == editor_type
-      resource_h[:test] = choices_to_test
+      resource_h[:exercises]&.each do |it|
+        language = it.dig(:language, :name) || resource_h.dig(:language, :name)
+        inflate_exercise! it, language, resource_h
+      end
+    end
+  end
+
+  class Choice < Exercise
+    def inflate_exercise!(exercise_h, language_name, guide_h)
+      return unless language_name == 'text'
+      return unless exercise_h[:editor] == editor_type
+      return if exercise_h[:test]
+
+      exercise_h[:test] = choices_to_test(exercise_h[:choices])
     end
   end
 
@@ -77,9 +88,9 @@ module Mumukit::Sync::Inflator
       'single_choice'
     end
 
-    def choices_to_test
-      choice = choices.find { |choice| choice['checked'] }
-      {'equal' => choice['value']}.to_yaml
+    def choices_to_test(choices)
+      choice = choices.find { |choice| choice[:checked] }
+      {'equal' => choice[:value]}.to_yaml
     end
   end
 
@@ -88,12 +99,48 @@ module Mumukit::Sync::Inflator
       'multiple_choice'
     end
 
-    def choices_to_test
+    def choices_to_test(choices)
       value = choices.each_with_index
-                .map { |choice, index| choice.merge('index' => index.to_s) }
-                .select { |choice| choice['checked'] }
-                .map { |choice| choice['index'] }.join(':')
+                .map { |choice, index| choice.merge(:index => index.to_s) }
+                .select { |choice| choice[:checked] }
+                .map { |choice| choice[:index] }.join(':')
       {'equal' => value}.to_yaml
     end
   end
+
+  ## TODO REMOVE
+  class GobstonesKidsBoards < Exercise
+    def inflate_exercise!(exercise_h, language_name, guide_h)
+      return unless language_name == 'gobstones'
+      return unless exercise_h[:layout] == 'input_kids'
+      return unless exercise_h[:test]
+
+      spec = YAML.load(exercise_h[:test])
+      example = spec&.dig('examples')&.first
+      with_head = spec&.dig('check_head_position')
+
+      return unless example
+
+      exercise_h[:initial_state] = initial_board example
+      exercise_h[:final_state] = final_board example, with_head
+    end
+
+    def to_gs_board(board, with_head)
+      "<gs-board#{with_head ? "" : " without-header"}> #{board} </gs-board>" if board
+    end
+
+
+    def initial_board(example)
+      to_gs_board(example['initial_board'], true)
+    end
+
+    def final_board(example, with_head)
+      to_gs_board(example['final_board'], with_head) || self.class.boom_board
+    end
+
+    def self.boom_board
+      "<img src='https://user-images.githubusercontent.com/1631752/37945593-54b482c0-3157-11e8-9f32-bd25d7bf901b.png'>"
+    end
+  end
+
 end
